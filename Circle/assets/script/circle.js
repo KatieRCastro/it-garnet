5eedc1e365171441c46d9560bb7b35ff7c437039
KatieRCastro <katxgina@gmail.com>
2024-02-07 09:22:30 +0000

    circle

--- Circle/assets/script/circle.js
+++ Circle/assets/script/circle.js
@@ -2,11 +2,9 @@ $("#CircleForm").validate();
 function displayCircle() {
   if ($("#CircleForm").valid()) {
     var radius = parseFloat(document.getElementById("radius").value);
-
     var diameter = 2 * radius;
     var circumference = 2 * Math.PI * radius;
     var area = Math.PI * radius * radius;
-
     document.getElementById("diameter").innerText = diameter.toFixed(2);
     document.getElementById("circumference").innerText =
       circumference.toFixed(2);
